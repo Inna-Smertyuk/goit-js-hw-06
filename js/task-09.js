4e12ebc7966cc21f7bf4a78222ad8e06b1c9b313
Inna-Smertyuk <86621124+Inna-Smertyuk@users.noreply.github.com>
2021-11-09 17:01:08 +0200

    hw-6

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const buttonEl = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+buttonEl.addEventListener('click', () => {
+
+    const getRandomColor = getRandomHexColor();
+
+    document.body.style.background = getRandomColor;
+    colorName.textContent = getRandomColor;
+});
